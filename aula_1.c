469bae3dc3d4c259ae1cc51434ff1c726fc8d8de
GabrielZuany <86385220+GabrielZuany@users.noreply.github.com>
2022-02-02 14:14:17 -0300

    Add files via upload

---
+++ aula_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> /*isso é standad in out*/ 
+//importar uma biblioteca
+int main ()
+{
+    printf("Hello World.\n"); // \n significa pular linha
+    printf("heheh to programando em C");
+    return 0;
+}
---
+++ aula_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int minhaidade = 19;
+    int dodoraidade = 18;
+    int laraidade = 15;
+
+    int idadetotal;
+    idadetotal = minhaidade + dodoraidade + laraidade;
+
+    printf("A soma das idades e %i.\n", idadetotal);
+
+    return 0;
+
+
+}
