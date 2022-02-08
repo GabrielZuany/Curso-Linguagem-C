6a55c409b5cb38cce9c03ed90b1eacca13b3e164
GabrielZuany <86385220+GabrielZuany@users.noreply.github.com>
2022-02-08 12:30:38 -0300

    Add files via upload

---
+++ aula_14(if-else).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int idade;
+
+    printf("Favor informar a idade: \n");
+    scanf("%i", &idade);
+
+    if(idade<18){
+        printf("Bebidas alcoolicas nao estao liberadas.\n");
+    }else{
+        printf("Bebidas alcoolicas estao liberadas.\n");
+    }
+
+    return  0;
+
+}
