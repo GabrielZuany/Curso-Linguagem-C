ce5a98dfe2829b73dd94c2de85d7c68e246977c9
GabrielZuany <86385220+GabrielZuany@users.noreply.github.com>
2022-02-20 11:21:00 -0300

    Delete aula1.c

--- aula1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> /*isso é standad in out*/ 
-//importar uma biblioteca
-int main ()
-{
-    printf("Hello World.\n"); // \n significa pular linha
-    printf("heheh to programando em C");
-    return 0;
-}
