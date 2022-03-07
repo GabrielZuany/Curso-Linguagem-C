b36d54fb85035ba3f688da73927113dbf79b8d31
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-07 10:23:16 -0300

    Adição de conversor de temperaturas e campo sair

--- Exercícios_em_C/Exercício(0)-Formulas_em_C.c
+++ Exercícios_em_C/Exercício(0)-Formulas_em_C.c
@@ -5,24 +5,26 @@ int main()
     float b; //base da fig plana
     float h;//altura da fig plana
     float raio;
-
     float a; //altura da figura espacial
     float area_base;
 
+    float celsius;
+    float fahrenheit;
+    float kelvin;
 
     char opcao; //Indica ao programa qual tipo de figura sera calculada
     int alternativa; //indica ao programa qual a figura que sera calculada
-
-    printf("Escolha o que deseja calcular: \n");
-    printf("a)Area; \nb)Volume;\n");
+    printf("Escolha o que deseja calcular:\n \n");
+    printf("a)Area; \nb)Volume;\nc)Converter undades de temperatura;\nd)Sair do programa.\n");
     scanf("%c", &opcao);
 
         switch (opcao)
         {
             case 'a': 
-                printf("1)Area do Quadrado;\n2)Area do Triangulo;\n3)Area do Retangulo;\n4)Area do Hexagono;\n5)Area do Circulo;");
+                printf("OBS: Para efeito de calculo assuma que: raiz3=1,7.\n");
+                printf("1)Area do Quadrado;\n2)Area do Triangulo;\n3)Area do Retangulo;\n4)Area do Hexagono;\n5)Area do Circulo;\n");
                 scanf("%d", &alternativa);
-
+                
                 switch(alternativa)
                 {
                     case 1: 
@@ -72,9 +74,10 @@ int main()
             break;
 
             case 'b':
+                printf("OBS: Para efeito de calculo assuma que: raiz3=1,7.\n");
                 printf("1)Volume do Cubo;\n2)Volume da Esfera;\n3)Volume do Prisma;\n\n");
                 scanf("%d", &alternativa);
-
+                ;
                 switch (alternativa)
                 {
                     case 1:
@@ -104,13 +107,55 @@ int main()
 
                     default:
                         printf("Erro! Por favor escolha uma das opcoes.\n");
-                }
-        
-            default:
-                printf("Erro! Por favor escolha uma das opcoes.\n");
-        }
+                }break;
             
-        printf("OBS: Para efeito de calculo assuma que: raiz3=1,7.\n");
+            case 'c':{
+                printf("1) Celsius para Fahrenheit e Kelvin;\n2)Fahrenheit para Celsius e Kelvin;\n3)Kelvin para Celsius e Fahrenheit.\n");
+                scanf("%d", &alternativa);
+                switch (alternativa)
+                {
+                    case 1:
+                        printf("Digite o valor da temperatura em Celsius: \n");
+                        scanf("%f", &celsius);
+                        fahrenheit = (celsius*1.8)+32;
+                        printf("%.2f Celsius = %.2f Fahrenheit.\n", celsius, fahrenheit);
+                        kelvin = celsius + 273.15;
+                        printf("%.2f Celsius = %.2f Kelvin.\n", celsius, kelvin);
+                    break;
 
+                    case 2:
+                        printf("Digite o valor da temperatura em Fahreheit: \n");
+                        scanf("%f", &fahrenheit);
+                        celsius=(fahrenheit-32)/1.8;
+                        printf("%.2f Fahrenheit = %.2f Celsius.\n", fahrenheit, celsius);
+                        kelvin = (fahrenheit+459.67)/1.8;
+                        printf("%.2f Fahrenheit = %.2f Kelvin.\n", fahrenheit, kelvin);
+                    break;
+
+                    case 3:
+                        printf("Digite o valor da temperatura em Kelvin: \n");
+                        scanf("%f", &kelvin);
+                        celsius =kelvin-273.15;
+                        printf("%.2f Kelvin = %.2f Celsius.\n", kelvin, celsius);
+                        fahrenheit = kelvin*1.8 - 459.67;
+                        printf("%.2f Kelvin = %.2f Fahrenheit.\n", kelvin, fahrenheit);
+                    break;
+
+                default: printf("ERRO! Por favor escolha uma das opcoes.");
+                }
+
+            }
+            break;
+
+            case 'd':{
+                    printf("Voce saiu do programa.\n");
+                }
+                
+            break;
+            default: printf("Erro! Por favor escolha uma das opcoes.\n\n");
+        }
+
+    printf("\nPrograma encerrado.\n");
+            
     return 0;
 }
