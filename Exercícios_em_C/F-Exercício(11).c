1fbb78cff82222cfc1999192217914d56c242941
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-12 10:43:13 -0300

    Upload de exercicio em desenvolvimento

--- Exercícios_em_C/F-Exercício(11).c
+++ Exercícios_em_C/F-Exercício(11).c
@@ -8,17 +8,18 @@ int main(void){
     int l;
     int c;
     int AlunosNotas[3][2]={0};
-    float media;
+    
 
     for(l=0;l<3;++l){
         printf("Digite a nota do aluno %i: \n", l+1);
         for(c=0;c<2 ;++c){
             scanf("%i", &AlunosNotas[l][c]);
-            media = l+c;
         }
-        media+=media;
-/*-----------*----------------*---------------*----------*/
+        
     }
+        
+/*-----------*----------------*---------------*----------*/
+   
     for(l=0;l<3;++l){
         printf("As notas do aluno %i foram: ", l+1);
         for(c=0;c<2 ;++c){
@@ -27,7 +28,6 @@ int main(void){
         printf("\n"); 
         }
 /*----------------*------------------*---------------------*/        
-   printf("A media dos alunos foi = %.2f", media);
 
     return 0;
 }
