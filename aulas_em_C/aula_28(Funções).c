845ddf020a79fc715fdc6aabfdc3bcaeec1fd778
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-17 10:42:46 -0300

    Adição de exemplos no repostório de aulas

---
+++ aulas_em_C/aula_28(Funções).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+
+    void imprimaMensagem(void); //função declarada
+    imprimaMensagem(); //chamar a função
+
+    return 0;
+}
+
+void imprimaMensagem(void) {
+
+    printf("teste");
+
+}
+
+
+
+/*
+#include<stdio.h>
+
+
+void imprimaMensagem(void) {
+
+    printf("teste");
+
+}
+
+int main(){
+
+    imprimaMensagem(); //chamar a função
+
+    return 0;
+}
+
+*/
---
+++ aulas_em_C/aula_29(Funções-pt.2).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void){
+
+    float calcularAreaRetangulo(float base, float altura);
+    float area = calcularAreaRetangulo(10.0, 20.0);
+
+    printf("A area = %f", area);
+
+    return 0;
+}
+
+float calcularAreaRetangulo(float base, float altura)
+
+/*float (o que a minha função RETORNA) cacularAreaRetangulo(nome da função) (Float base, float altura (o qu a funçao RECEBE de informação)) */
+
+{
+    float area = base*altura;
+
+    return area; 
+}
