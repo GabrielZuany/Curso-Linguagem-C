d9179688b6182982914a59ca945f698e25a2a541
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-21 08:55:29 -0300

    Upload de aulas sobre funções em C

---
+++ aulas_em_C/aula_36(Argumentos-Vetores-Funções).c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void){
+
+    int vetor[10]={6, 5, 4, 3, 2, 1, 0, 7, 9, 8};
+    int i;
+    void ordemCrescente(int vetor[], int n);//ao declarar vetor como argumento, nao precisa indicar o numero de itens do vetor
+
+
+    ordemCrescente(vetor, 10);//declarar o nome do vetor e o numero
+
+    for(i=0; i<10; ++i){
+            printf("%i ", vetor[i]);
+    }
+
+    system ("pause");
+    return 0;
+}
+
+void ordemCrescente(int vetor[], int n){
+    int i, j, temporaria;
+
+    for(i=0;i<n;++i){
+        for(j= i+1; j<n;++j){
+            if(vetor[i]>vetor[j]){
+
+                temporaria=vetor[i];
+                vetor[i]=vetor[j];
+                vetor[j]=temporaria;
+            }
+
+        }
+    }
+    
+
+}
---
+++ aulas_em_C/aula_38(Comportamento-de-Argumentos-em-Funções).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void){
+    void funcaoPrint(int x, int vetor[]);
+    int x =10;
+    int vetor[3]={10};
+
+    funcaoPrint(x, vetor);
+
+    printf("Variavel int na funcao principal = %i \n", x);
+    printf("Vetor na funcao principal = %i \n", vetor[1]);
+
+    system("pause");
+    return 0;
+}
+
+void funcaoPrint(int x, int vetor[]){
+    x= x+10;
+    vetor[1]=20;
+
+    printf("Variavel int na funcao secundaria = %i \n", x);
+    printf("Vetor na funcao secundaria = %i \n", vetor[1]);
+}
