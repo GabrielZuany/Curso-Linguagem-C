c03de9baa6782bb99daa7722117a0ffc88648c78
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-23 10:07:44 -0300

    Adição de aulas de estruturas em c

---
+++ aulas_em_C/aula_39(Estruturas).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void){
+
+    struct horario {
+        int hora;
+        int minuto;
+        int segundo;
+    };
+    
+    struct horario agora;
+
+    agora.hora=15;
+    agora.minuto=17;
+    agora.segundo=30;
+
+    printf("%i:%i:%i\n", agora.hora, agora.minuto, agora.segundo);
+
+    system("pause");
+    return 0;
+}
+/*
+    struct DEFINIÇÃO{
+
+        tipos de data(int float char double.....)
+
+    };
+
+    struct definição NOME DA ESTRUTURA; estrutura declarada.
+
+    nomeDaEstrutura.variavel dentro da estrutura = X;
+
+    */
---
+++ aulas_em_C/aula_40(Manipular-estruturas).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void){
+
+    struct horario {
+        int hora;
+        int minuto;
+        int segundo;
+        double teste;
+        char letra;
+    };
+    
+    struct horario agora;
+    agora.hora=15;
+    agora.minuto=17;
+    agora.segundo=30;
+
+    struct horario depois;
+    depois.hora = agora.hora+10;
+    depois.minuto = agora.minuto;
+    depois.teste = 50.55/123;
+    depois.letra='a';
+
+    printf("%i\n", depois.hora);
+    printf("%i\n", depois.minuto);
+    printf("%d\n", depois.teste);
+    printf("%c\n", depois.letra);
+    
+
+    system("pause");
+    return 0;
+}
