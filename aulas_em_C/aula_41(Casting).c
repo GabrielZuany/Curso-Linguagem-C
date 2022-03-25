72fd6c0ebe55b925e9074078abd80ec560bac97f
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-24 22:38:23 -0300

    Upload de exemplo de Casting em C

---
+++ aulas_em_C/aula_41(Casting).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+    int x =16;
+    int y = 3;
+
+    float resultado = (float)x/y; /*ao colocar (tipo)x/y, eu forcei a linguagem a entender o valor x como float, mesmo que ele tenha sido definido como int. Isso é o casting*/
+
+    printf("%f\n", resultado);
+
+    system("pause");
+    return 0;
+}
