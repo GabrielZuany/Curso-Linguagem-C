0a6fdfedf6e7820f79e952f6970791b8f613cf12
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-03-31 10:52:15 -0300

    Adição de exercicios

---
+++ Exercícios_em_C/A)Exercício(8.2)Matriz-vetor.c
@@ -0,0 +1,34 @@
+/*
+Faça um vetor A tipo inteiro que armazene 20 valores. Em seguida faça um vetor B que armazene outros 2 valores. Agora, faça um vetor C que contenha 20 valores, os quais deverão ser vetor A[n]- vetor B[n]
+*/
+#include <stdio.h>
+
+int main(void){
+
+    int vetor_A[20];
+    int vetor_B[20];
+    int vetor_C[20];
+
+    int i;
+
+    printf("Digite 20 valores para adicionar ao vetor A: \n");
+    for(i=0; i<20;++i){
+        scanf("%i", &vetor_A[i]);
+    }
+
+    printf("Digite 20 valores para adicionar ao vetor B: \n");
+    for(i=0; i<20;++i){
+        scanf("%i", &vetor_B[i]);
+    }
+
+    printf("O vetor C (vetor A-vetor B) e = ");
+    for(i=0; i<20;++i){
+        vetor_C[i]=vetor_A[i]-vetor_B[i];
+        printf("|%i|", vetor_C[i]);
+    }
+
+    printf("\n");
+    
+    system("pause");
+    return 0;
+}
---
+++ Exercícios_em_C/A)Exercício(8.3)Matriz-vetor.c
@@ -0,0 +1,29 @@
+/*
+Faça um programa que possua um vetor com 10 posições. Peça ao usuario para digitar dez valores e exiba quais valores sao ímpares.
+*/
+
+#include<stdio.h>
+
+int main(void){
+
+    int vetor[10];
+    int valor;
+    int i;
+
+    printf("Digite 10 valores para armazenar no vetor: \n");
+
+    for(i=0;i<10;++i){
+        scanf("%i", &vetor[i]);
+    }
+
+    for(i=0;i<10;++i){
+        valor = vetor[i]%2;
+        printf("Sao impares os valores: ");
+        if(valor!=0){
+            printf("|%i|\n", vetor[i]);
+        }
+
+    }
+    system("pause");
+    return 0;
+}
