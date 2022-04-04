a9afd005185ba985d34a2d1cfee8daa8a0f20fa6
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-04-03 21:19:20 -0300

    Upload de exercicio de ordenação de vetores

---
+++ Exercícios_em_C/A)Exercício(8.4)Matriz-vetor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    
+    int vetor[12];
+    int i, j;
+    int temporaria;
+
+    printf("Digite 12 valores: \n");
+
+    for(i=0;i<12;++i){
+        scanf("%i", &vetor[i]);
+    }
+
+    for(i=0;i<12;++i){
+        for(j=i+1;j<12;++j){
+            if(vetor[i]>vetor[j]){
+                temporaria = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temporaria;
+            }
+        }
+    }
+    for(i=0;i<12;++i){
+        printf("|%i|", vetor[i]);
+    }
+    
+    system("pause");
+    return 0;
+}
